994a5c043887e1188bfc14553cefbd5e73db5185
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2019-07-10 11:53:36 +0200

    Improve example for update product attributes

--- PixiApiCustomize/Observer/ImportStockProductBeforeSave.php
+++ PixiApiCustomize/Observer/ImportStockProductBeforeSave.php
@@ -37,8 +37,9 @@ class ImportStockProductBeforeSave extends AbstractObserver
      */
     public function execute(Observer $observer)
     {
-        // Any change in EAN or active state? Is it necessary to save the product?
+        // Any change in EAN or active state? Is it necessary to save the product attribute?
         $perform_product_save = $observer->getData('perform_product_save');
+        $attribute_data = $observer->getData('attribute_data');
 
         // get product.
         $product = $observer->getData('product');
@@ -51,7 +52,15 @@ class ImportStockProductBeforeSave extends AbstractObserver
         }
         $this->pixiLogger->info('Status from Product', ['SKU' => $product->getSku(), 'Status' => $product->getStatus()]);
 
-        // Force to save product if anything changed in this observer
-        //   $observer->setData('perform_product_save', true);
+        $this->pixiLogger->info('Product Attribute', $attribute_data);
+        // for example change the weight attribute from product
+        $attribute_data['weight'] = 123.0;
+        $observer->setData('attribute_data', $attribute_data);
+
+        // Force to save product attribute if anything changed in this observer
+        if (!$perform_product_save) {
+            $observer->setData('perform_product_save', true);
+        }
+
     }
 }
